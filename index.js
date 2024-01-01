cae8e7ec8fb2e782da474450ee1d67679fd3c016
varunkumarmedam <varunkumarmedam@gmail.com>
2024-01-02 01:34:26 +0530

    allowing cors request from all origins

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 8080;
+const cors = require('cors');
 
 var currencyRouter = require("./routes/currency.routes");
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
